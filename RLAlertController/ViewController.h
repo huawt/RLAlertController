ecdb2ae077044efc5d31f303b93f76e5c9068547
华文涛 <huawentao@huiwenliye.com>
2017-08-18 09:39:24 +0800

    Initial Commit

---
+++ RLAlertController/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  RLAlertController
+//
+//  Created by 华文涛 on 2017/8/18.
+//  Copyright © 2017年 WinTer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ RLAlertController/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  RLAlertController
+//
+//  Created by 华文涛 on 2017/8/18.
+//  Copyright © 2017年 WinTer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
